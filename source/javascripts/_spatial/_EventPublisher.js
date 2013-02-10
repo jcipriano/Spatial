30b249ae36659d65ba43074e900ca35fab614672
jcipriano <cipriano.jonathan@gmail.com>
2013-02-10 12:42:52 -0800

    Refactorig class names.

---
+++ source/javascripts/_spatial/_EventPublisher.js
@@ -0,0 +1,49 @@
+Spatial.Events = {
+  PLAY: 'play',
+  STOP: 'stop',
+  PROCESSED: 'processed',
+  LOADED: 'loaded',
+  SUCCESS: 'success',
+  FAILURE: 'failure'
+};
+
+Spatial.EventPublisher = function() {
+  this.events = { };
+};
+
+Spatial.EventPublisher.prototype.publish = function(event, data) {
+  var i, scope, callback, callbacks, _results;
+    
+  if (this.events && this.events[event]) {
+    callbacks = this.events[event];
+      
+    i = 0;
+    _results = [];
+    while (i < callbacks.length) {
+      scope = callbacks[i].obj;
+      callback = callbacks[i].func;
+      callback.apply(scope ? scope : this, [data]);
+      _results.push(i++);
+    }
+      
+    return _results;
+  }
+};
+
+Spatial.EventPublisher.prototype.add = function(event, callback, scope) {
+  if (!this.events[event]){
+    this.events[event] = [];
+  }
+    
+  return this.events[event].push({ func: callback, obj: scope });
+};
+  
+Spatial.EventPublisher.prototype.remove = function(event, callback) {
+  var index, callbacks;
+    
+  if (this.events && this.events[event]) {
+    callback = this.events[event];
+    index = callbacks.indexOf(callback);
+    return callbacks.splice(index, 1);
+  }
+};
