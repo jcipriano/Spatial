c9ec76b12947f6aa72bd08eeebcfac2f5a674a31
Jonathan Cipriano <cipriano.jonathan@gmail.com>
2013-02-15 15:27:41 -0800

    Adding more levels

--- source/javascripts/spatial.config.js
+++ source/javascripts/spatial.config.js
@@ -6,13 +6,48 @@ Spatial.Config = {
       'cubeLength': 3
     },{
       'name': 'Level 2',
-      'structures': 5,
+      'structures': 3,
       'cubeSize': 10,
-      'cubeLength': 6
+      'cubeLength': 5
     },{
       'name': 'Level 3',
       'structures': 3,
-      'cubeSize': 5,
+      'cubeSize': 10,
+      'cubeLength': 7
+    },{
+      'name': 'Level 4',
+      'structures': 5,
+      'cubeSize': 10,
+      'cubeLength': 7
+    },{
+      'name': 'Level 5',
+      'structures': 5,
+      'cubeSize': 10,
+      'cubeLength': 9
+    },{
+      'name': 'Level 6',
+      'structures': 3,
+      'cubeSize': 7,
+      'cubeLength': 12
+    },{
+      'name': 'Level 7',
+      'structures': 3,
+      'cubeSize': 7,
       'cubeLength': 15
+    },{
+      'name': 'Level 8',
+      'structures': 5,
+      'cubeSize': 7,
+      'cubeLength': 15
+    },{
+      'name': 'Level 9',
+      'structures': 5,
+      'cubeSize': 5,
+      'cubeLength': 20
+    },{
+      'name': 'Level 10',
+      'structures': 5,
+      'cubeSize': 5,
+      'cubeLength': 25
     }]
 };
