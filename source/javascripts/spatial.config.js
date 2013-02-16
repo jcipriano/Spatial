f79305f8c7b93dc5b19e40d28da0dbd88030f1d4
jcipriano <cipriano.jonathan@gmail.com>
2013-02-16 11:46:13 -0800

    Git ignore update.

--- source/javascripts/spatial.config.js
+++ source/javascripts/spatial.config.js
@@ -1,53 +1,124 @@
 Spatial.Config = {
+  'audio': [{
+    'id': 'voice_level01',
+    'source': 'audio/voice_level01.mp3'
+  },{
+    'id': 'voice_level02',
+    'source': 'audio/voice_level02.mp3'
+  },{
+    'id': 'voice_level03',
+    'source': 'audio/voice_level03.mp3'
+  },{
+    'id': 'voice_level04',
+    'source': 'audio/voice_level04.mp3'
+  },{
+    'id': 'voice_level05',
+    'source': 'audio/voice_level05.mp3'
+  },{
+    'id': 'voice_level06',
+    'source': 'audio/voice_level06.mp3'
+  },{
+    'id': 'voice_level07',
+    'source': 'audio/voice_level07.mp3'
+  },{
+    'id': 'voice_level08',
+    'source': 'audio/voice_level08.mp3'
+  },{
+    'id': 'voice_level09',
+    'source': 'audio/voice_level09.mp3'
+  },{
+    'id': 'voice_level10',
+    'source': 'audio/voice_level10.mp3'
+  }],
   'levels': [{
-      'name': 'Level 1',
-      'structures': 3,
-      'cubeSize': 10,
-      'cubeLength': 3
-    },{
-      'name': 'Level 2',
-      'structures': 3,
-      'cubeSize': 10,
-      'cubeLength': 5
-    },{
-      'name': 'Level 3',
-      'structures': 3,
-      'cubeSize': 10,
-      'cubeLength': 7
-    },{
-      'name': 'Level 4',
-      'structures': 5,
-      'cubeSize': 10,
-      'cubeLength': 7
-    },{
-      'name': 'Level 5',
-      'structures': 5,
-      'cubeSize': 10,
-      'cubeLength': 9
-    },{
-      'name': 'Level 6',
-      'structures': 3,
-      'cubeSize': 7,
-      'cubeLength': 12
-    },{
-      'name': 'Level 7',
-      'structures': 3,
-      'cubeSize': 7,
-      'cubeLength': 15
-    },{
-      'name': 'Level 8',
-      'structures': 5,
-      'cubeSize': 7,
-      'cubeLength': 15
-    },{
-      'name': 'Level 9',
-      'structures': 5,
-      'cubeSize': 5,
-      'cubeLength': 20
-    },{
-      'name': 'Level 10',
-      'structures': 5,
-      'cubeSize': 5,
-      'cubeLength': 25
-    }]
+    'name': 'Level 1',
+    'structures': 3,
+    'cubeSize': 10,
+    'cubeLength': 3,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 2',
+    'structures': 3,
+    'cubeSize': 10,
+    'cubeLength': 5,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 3',
+    'structures': 3,
+    'cubeSize': 10,
+    'cubeLength': 7,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 4',
+    'structures': 5,
+    'cubeSize': 10,
+    'cubeLength': 7,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 5',
+    'structures': 5,
+    'cubeSize': 10,
+    'cubeLength': 9,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 6',
+    'structures': 3,
+    'cubeSize': 7,
+    'cubeLength': 12,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 7',
+    'structures': 3,
+    'cubeSize': 7,
+    'cubeLength': 15,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 8',
+    'structures': 5,
+    'cubeSize': 7,
+    'cubeLength': 15,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 9',
+    'structures': 5,
+    'cubeSize': 5,
+    'cubeLength': 20,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  },{
+    'name': 'Level 10',
+    'structures': 5,
+    'cubeSize': 5,
+    'cubeLength': 25,
+    'audio': {
+      'intro': 'voice_level01',
+      'song': '',
+    }
+  }]
 };
